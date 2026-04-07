ee5548cab73d1c9aff378fd49bb75923ec69b959
shanu <ashanu2004@gmail.com>
2026-04-07 14:59:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -6,16 +6,6 @@ import About from "./pages/About/Page";
 import Services from "./pages/Services/Page";
 import Projects from "./pages/Projects/Page";
 import Contact from "./pages/Contact/Page";
-import Light from "./pages/Services/Light";
-import Video from "./pages/Services/Video";
-import Staging from "./pages/Services/Staging";
-import Audio from "./pages/Services/Audio";
-import Ceremonies from "./pages/Projects/Ceremonies";
-import ConcertsFestivals from "./pages/Projects/ConcertsFestivals";
-import SpecialEvents from "./pages/Projects/SpecialEvents";
-import Sports from "./pages/Projects/Sports";
-import CorporateEvents from "./pages/Projects/CorporateEvents";
-import TradeShows from "./pages/Projects/TradeShows";
 
 function App() {
   return (
@@ -26,23 +16,8 @@ function App() {
 
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          {/* Services Parent */}
-          <Route path="/services" element={<Services />}>
-            <Route path="light" element={<Light />} />
-            <Route path="audio" element={<Audio />} />
-            <Route path="video" element={<Video />} />
-            <Route path="staging" element={<Staging />} />
-          </Route>
-
-          <Route path="/projects" element={<Projects />}>
-            <Route path="ceremonies" element={<Ceremonies />} />
-            <Route path="concerts-festivals" element={<ConcertsFestivals />} />
-            <Route path="special-events" element={<SpecialEvents />} />
-            <Route path="sports" element={<Sports />} />
-            <Route path="corporate-events" element={<CorporateEvents />} />
-            <Route path="trade-shows" element={<TradeShows />} />
-          </Route>
-
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/services" element={<Services />} />
           <Route path="/contact" element={<Contact />} />
 
         </Route>
