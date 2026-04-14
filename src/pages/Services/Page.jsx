d3dd56603f23c67a1689cfdce384b870412b7d80
shanu <ashanu2004@gmail.com>
2026-04-14 10:19:51 +0530

    fix the issue

--- src/pages/Services/Page.jsx
+++ src/pages/Services/Page.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+// import { useLocation } from "react-router-dom";
 
 import Serviceheader from "./Components/Serviceheader";
-import Servicescontent from "./Components/Servicescontent";
+// import Servicescontent from "./Components/Servicescontent";
 import Servicecontent from "./Components/Servicecontent";
 import Serviceanimation from "./Serviceanimation";
 
 const Page = () => {
-  const location = useLocation();
-  const selectedService = location.state?.service;
+  // const location = useLocation();
+  // const selectedService = location.state?.service;
 
   return (
     <div>
@@ -17,7 +17,7 @@ const Page = () => {
       <Serviceanimation />
 
       {/* ✅ pass selected service */}
-      <Servicescontent selectedService={selectedService} />
+      {/* <Servicescontent selectedService={selectedService} /> */}
     </div>
   );
 };
